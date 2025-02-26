98652f3a2cf79beb89cdf0eaba93f7694ae14c44
momoo038 <196245462+momoo038@users.noreply.github.com>
2025-02-26 15:19:57 +0100

    Finished RPS Console game

---
+++ script.js
@@ -0,0 +1,65 @@
+// create function that returns 3 choices at random
+function getComputerChoice(){
+// use math.random to return the 3 said choices
+    let computerChoice = Math.floor((Math.random() * 3) + 1);
+
+    switch(computerChoice) {
+        case 1:
+            return "rock";
+        case 2:
+            return "paper";
+        case 3:
+            return "scissors";
+    }
+}
+
+// get user input to make their own choice out of those 3 choices
+function getUserChoice(){
+    let userChoice = prompt("What do you choose? [rock-paper-scissors]")
+    return userChoice;
+}
+// return the user choice
+//
+// create variable to store human score
+let humanScore = 0;
+// create variable to store computer score
+let computerScore = 0;
+
+// function that takes human and computer choices as arguments and plays round, increments round winner score and logs the winner
+function playRound(computerChoice, userChoice){
+// make user input case insensitive
+    userChoice = userChoice.toLowerCase();
+// uh.. make the game
+    if(userChoice === computerChoice){
+        console.log("It's a tie!")
+    } else if (
+        (userChoice === "rock" && computerChoice === "scissors") ||
+        (userChoice === "paper" && computerChoice === "rock") ||
+        (userChoice === "scissors" && computerChoice === "paper")
+    ) {
+        console.log("You win!")
+        humanScore++;
+    } else {
+        console.log("Computer wins!")
+        computerScore++;
+    }
+
+}
+
+// variable stores in the choices
+const userSelection = getUserChoice();
+const computerSelection = getComputerChoice();
+
+// full game
+function playGame(){
+// for loop that plays as long as i (round number) is below 5 and logs user and computer scores
+    for(let i = 0; i < 5; i++){
+        console.log(`Round ${i + 1}`);
+        const userSelection = getUserChoice();
+        const computerSelection = getComputerChoice();
+        playRound(computerSelection, userSelection);
+    }
+    console.log(`Final Score: Human: ${humanScore}, Computer: ${computerScore}`);
+}
+// play game
+playGame();
